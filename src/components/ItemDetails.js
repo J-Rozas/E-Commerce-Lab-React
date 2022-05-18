9fe003cbc7455404756c8852b6239f8f6104701a
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-18 13:39:57 +0100

    Create all components for items

---
+++ src/components/ItemDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ItemDetail = () => {
+  return (
+    <>
+      <p>
+        detail
+      </p>
+    </>
+  )
+}
+
+export default ItemDetail;
---
+++ src/components/ItemsList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemDetail from "./ItemDetails";
+
+const ItemsList = () => {
+  return (
+    <>
+      <p>
+        An item
+      </p>
+    </>
+  )
+}
+
+export default ItemsList;
