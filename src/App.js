2a322da3c3fc393395e3d2daafc827df0df1f8f4
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-18 13:37:09 +0100

    Add container for items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
 import './App.css';
+import ItemsContainer from './containers/ItemsContainer';
 
 function App() {
   return (
-    <h1>Test</h1>
+    <>
+      <h1>Test</h1>
+      <ItemsContainer></ItemsContainer>
+    </>
   );
 }
 
---
+++ src/containers/ItemsContainer.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+
+const ItemsContainer = () => {
+  return (
+    <>
+      <h2>Hi</h2>
+    </>
+  )
+}
+
+export default ItemsContainer
